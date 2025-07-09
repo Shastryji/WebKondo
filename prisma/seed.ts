385e38d5b56d49a82b6e611b72c9961888d1d69f
Shastryji <shivam1234shastry@gmail.com>
2025-07-10 00:30:17 +0530

    added the db.ts for accessing the database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,51 +1,27 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Prisma} from '../src/generated/prisma'
 const prisma = new PrismaClient()
-async function main() {
-  const alice = await prisma.user.upsert({
-    where: { email: 'alice@prisma.io' },
-    update: {},
-    create: {
-      email: 'alice@prisma.io',
-      name: 'Alice',
-      posts: {
-        create: {
-          title: 'Check out Prisma with Next.js',
-          content: 'https://www.prisma.io/nextjs',
-          published: true,
-        },
-      },
-    },
-  })
-  const bob = await prisma.user.upsert({
-    where: { email: 'bob@prisma.io' },
-    update: {},
-    create: {
-      email: 'bob@prisma.io',
-      name: 'Bob',
-      posts: {
+
+const userData : Prisma.UserCreateInput[] =[{
+    name: "Alica",
+    email: "alice@prisma.io",
+    posts: {
         create: [
-          {
-            title: 'Follow Prisma on Twitter',
-            content: 'https://twitter.com/prisma',
-            published: true,
-          },
-          {
-            title: 'Follow Nexus on Twitter',
-            content: 'https://twitter.com/nexusgql',
-            published: true,
-          },
+            {title: "join the prisma discord",
+            content: "https://pris.ly/discord",
+            published: true},
+            {title: "join the prisma yt",
+            content: "https://pris.ly/youtube"
+            },
         ],
-      },
     },
-  })
-  console.log({ alice, bob })
+}]
+
+export async function main()
+{
+    for(const u of userData)
+    {
+        await prisma.user.create({data : u})
+    }
 }
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+
+main();
