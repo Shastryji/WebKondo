fa06376aa3baab2e1c8e1fb36380bbbb6b7b3930
Shastryji <shivam1234shastry@gmail.com>
2025-07-21 21:14:13 +0530

    inngest

--- src/trpc/server.tsx
+++ src/trpc/server.tsx
@@ -2,8 +2,8 @@ import 'server-only'; // <-- ensure this file cannot be imported from the client
 import { createTRPCOptionsProxy } from '@trpc/tanstack-react-query';
 import { cache } from 'react';
 import { createTRPCContext } from './init';
-import { makeQueryClient } from './query-client';
 import { appRouter } from './routers/_app';
+import { makeQueryClient } from './routers/query-client';
 // IMPORTANT: Create a stable getter for the query client that
 //            will return the same client during the same request.
 export const getQueryClient = cache(makeQueryClient);
